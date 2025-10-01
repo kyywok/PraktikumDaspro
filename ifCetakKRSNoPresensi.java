26d1c6c0cb29b1ff73a40dada36baf03c138d3ac
Aqillah <aqillahzakkyramadana@gmail.com>
2025-10-01 08:31:59 +0700

    percobaan1

---
+++ ifCetakKRSNoPresensi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ifCetakKRSNoPresensi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = input.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+
+    }
+}
